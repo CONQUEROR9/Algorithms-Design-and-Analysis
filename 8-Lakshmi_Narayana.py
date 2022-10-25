35f05507b34bcb1e4cca52020312a8cfde521f5c
Lakshmi Narayana Naidu <52100527+CONQUEROR9@users.noreply.github.com>
2022-10-25 09:14:59 +0530

    Create 8-Lakshmi_Narayana.py

---
+++ 8-Lakshmi_Narayana.py
@@ -0,0 +1,31 @@
+adjacency_matrix = [[0, 1, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 1, 0, 0, 1],
+                    [0, 0, 0, 1, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 1, 0],
+                    [0, 0, 1, 1, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 1, 0, 0]
+                    ]
+
+
+def dfs(adj_matrix):
+    visited = set()
+    for row in range(len(adj_matrix)):
+        for col in range(len(adj_matrix[0])):
+            if (row, col) not in visited:
+                nodes = [(row, col)]
+                while nodes:
+                    row, col = nodes.pop()
+                    if row >= len(adj_matrix) or col >= len(adj_matrix[0]) or row < 0 or col < 0:
+                        continue
+                    if (row, col) not in visited:
+                        if adj_matrix[row][col] == 1:
+                            visited.add((row, col))
+                            nodes.append((row + 1, col))
+                            nodes.append((row, col + 1))
+                            nodes.append((row - 1, col))
+                            nodes.append((row, col - 1))
+    print(visited)
+
+
+dfs(adjacency_matrix)
